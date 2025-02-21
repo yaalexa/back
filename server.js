db4265bf4ea53b9c2abf5adaa7792a9c44f8e92d
yaalexa25 <yaalexa25@gmail.com>
2025-02-21 00:51:03 -0500

    cambio

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const { sequelize } = require("./models");
+
 const usersRouter = require("./app/routers/usersRouter");
 dotenv.config();
 console.log("Servidor iniciando...");  // <-- Log para ver si el servidor arranca
@@ -8,24 +8,13 @@ console.log("Servidor iniciando...");  // <-- Log para ver si el servidor arranc
 const app = express();
 app.use(express.json());
 
-// Middleware para capturar errores globales
-app.use((err, req, res, next) => {
-    console.error("Error en el servidor:", err);
-    res.status(500).json({ error: "Error interno del servidor" });
-});
 
-// Rutas
 
 app.use("/api", (req, res) => {
   res.json({ msj: "aqui" });
 });
 
 
-
-sequelize.authenticate()
-  .then(() => console.log("Conexión a MySQL establecida correctamente"))
-  .catch(err => console.error("Error al conectar con MySQL:", err));
-// Configuración del puerto
 const PORT = process.env.PORT ;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
