204c6fee893f7e9d28424d7a43dec5162bd95746
yaalexa25 <yaalexa25@gmail.com>
2025-02-21 01:48:41 -0500

    comit

--- models/index.js
+++ models/index.js
@@ -5,12 +5,19 @@ const { Sequelize } = require("sequelize");
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
 
-const sequelize = new Sequelize(config.database, config.username, config.password, {
-  ...config,
+// En lugar de importar desde config.json, leer las variables de entorno directamente
+const config = {
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "JoATKoABJBbywoWYCTPluQRQBgpljbDA",
+  database: process.env.DB_DATABASE || "railway",
+  host: process.env.DB_HOST || "localhost",
+  dialect: process.env.DB_DIALECT || "mysql",
+  port: process.env.DB_PORT || "3306",
   dialectModule: mysql2
-});
+};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 const db = {};
 
