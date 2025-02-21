ed63ce9d297c513112e821b1e31aa4a4c8a85134
yaalexa25 <yaalexa25@gmail.com>
2025-02-21 00:40:02 -0500

    comit

--- models/index.js
+++ models/index.js
@@ -9,14 +9,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], {
-    dialect: config.dialect,
-    host: config.host,
-    port: config.port,
-  });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
