51c7fa899601cc833e40f5e5810baf1719f03221
yaalexa25 <yaalexa25@gmail.com>
2025-02-20 23:53:44 -0500

    first commit

---
+++ app/routers/usersRouter.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+let UserController=require("../controllers/userController")
+// rutas user
+router.get('/user', UserController.obtenerUsuarios);
+router.post('/login', UserController.login);
+router.post('/user', UserController.register);
+router.delete('/user/:id', UserController.eliminarUsuarios);
+router.put('/user/:id', UserController.actualizarUsuarios);
+
+module.exports = router;
