657c77c17feaf08b615fc202c6cef44faddd738d
yaalexa25 <yaalexa25@gmail.com>
2025-02-24 16:30:49 -0500

    cambio

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const usersRouter = require("./routers/usersRouter");
-
 dotenv.config();
+
 const app = express();
+app.use(express.json());
 
-const cors = require("cors");
-const allowedOrigins = ['http://localhost:5173'];
-
-app.use(cors({
-    origin: '*',  // 👈 Agrega explícitamente localhost
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true // 👈 Importante para autenticación
-}));
-
-app.use(express.json()); // 👈 Solo aquí
 
 app.use("/api", usersRouter);
 
-const PORT = process.env.PORT || 3000;
+// Configuración del puerto
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
-    console.log(`✅ Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
